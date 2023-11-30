fcf28fcc349ee789b687e361eec791fafae01562
Abdesselam Benameur <abdesselam.benameur@etu.u-paris.fr>
2023-11-30 15:42:13 +0100

    Update main.py

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ import numpy as np
 import prince
 import umap
 from sklearn.cluster import KMeans
+import matplotlib.pyplot as plt
 
 def dim_red(mat, p, method):
     '''
@@ -21,26 +22,26 @@ def dim_red(mat, p, method):
     '''
     if method=='ACP':
         pca = prince.PCA(
-        n_components=p,
-        n_iter=3,
-        rescale_with_mean=True,
-        rescale_with_std=True,
-        copy=True,
-        check_input=True,
-        engine='sklearn',
-        random_state=42
+            n_components=p,
+            n_iter=3,
+            rescale_with_mean=True,
+            rescale_with_std=True,
+            copy=True,
+            check_input=True,
+            engine='sklearn',
+            random_state=42
         )
         pca = pca.fit(mat)    
         red_mat = pca.transform(mat)
         
     elif method=='TSNE':
-        tsne = TSNE(n_components=2, verbose=1, perplexity=40, n_iter=300)
+        tsne = TSNE(n_components=2, perplexity=40, n_iter=300)
         red_mat = tsne.fit_transform(mat)
         
     elif method=='UMAP':
         reducer = umap.UMAP(n_components=p)
         red_mat = reducer.fit_transform(mat)
-        
+    
     else:
         raise Exception("Please select one of the three methods : APC, AFC, UMAP")
     
@@ -61,7 +62,7 @@ def clust(mat, k):
     '''
     
     # Kmeans
-    kmeans = KMeans(n_clusters=k, random_state=0).fit(mat)
+    kmeans = KMeans(n_clusters=k, random_state=0, n_init="auto").fit(mat)
     pred = kmeans.labels_
     
     return pred
@@ -73,14 +74,23 @@ labels = ng20.target[:2000]
 k = len(set(labels))
 
 # embedding
-model = SentenceTransformer('paraphrase-MiniLM-L6-v2')
-embeddings = model.encode(corpus)
+# model = SentenceTransformer('paraphrase-MiniLM-L6-v2')
+# embeddings = model.encode(corpus)
+# load embeddings
+embeddings = np.load('embeddings.npy')
+import pandas as pd
+embeddings = pd.DataFrame(embeddings)
 
 # Perform dimensionality reduction and clustering for each method
-methods = ['ACP', 'AFC', 'UMAP']
+methods = ['ACP', 'TSNE', 'UMAP', None]
 for method in methods:
-    # Perform dimensionality reduction
-    red_emb = dim_red(embeddings, 20, method)
+    if method is None:
+        print(f'Method: Without dimensionality reduction')
+        red_emb = embeddings
+    else:
+        print(f'Method: {method}')
+        # Perform dimensionality reduction
+        red_emb = dim_red(embeddings, 20, method)
 
     # Perform clustering
     pred = clust(red_emb, k)
@@ -90,5 +100,9 @@ for method in methods:
     ari_score = adjusted_rand_score(pred, labels)
 
     # Print results
-    print(f'Method: {method}\nNMI: {nmi_score:.2f} \nARI: {ari_score:.2f}\n')
-
+    red_emb = pd.DataFrame(red_emb)
+    print(f'NMI: {nmi_score:.2f} \nARI: {ari_score:.2f}\n')
+    # Plot results (save as png)
+    plt.scatter(red_emb[0], red_emb[1], c=pred)
+    plt.savefig(f'clustering_{method}.png')
+    plt.close()
